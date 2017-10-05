415a1105e97179cb14fbbf92d9fe7c18a6c210c6
JaredTCan <jaredtcan@gmail.com>
2017-10-05 19:47:44 +0000

    added cgi-bin for website scripts, added to gitToApache so cgi-bin is synced with apache same as the document root. Added helloWorld.py test cgi script and contents of apache/cgi-bin to cgi-bin

--- gitScripts/gitToApache.sh
+++ gitScripts/gitToApache.sh
@@ -12,28 +12,49 @@ echo "Pulling data from git"
 cd /home/pi/Pi-in-the-Sky/
 git pull 
 
-echo "checking for changes"
+echo "checking for changes in documentRoot"
 DIFF=$(diff -r /home/pi/Pi-in-the-Sky/documentRoot/ /usr/local/apache/htdocs/)
 if [ "$DIFF" == "" ]
 then
 	echo "no changes"
-	exit 0
+
+else
+	echo"Tarring git/documentRoot"
+	cd /home/pi/Pi-in-the-Sky/documentRoot/
+	tar -cvf documentRoot.tar .
+
+	echo "cleaning apache/htdocs"
+	cd /usr/local/apache/htdocs/
+	rm -rf * 
+
+	echo "Unpacking"
+	mv /home/pi/Pi-in-the-Sky/documentRoot/documentRoot.tar ./
+	tar -xvf documentRoot.tar
+
+	echo "Cleaning up"
+	rm documentRoot.tar
 fi
+  
+echo "checking for changes in cgi-bin"
+DIFF=$(diff -r /home/pi/Pi-in-the-Sky/cgi-bin/ /usr/local/apache/cgi-bin/)
+if [ "$DIFF" == "" ]
+then
+	echo "no changes"
+else
+	echo "Tarring git/cgi-bin"
+	cd /home/pi/Pi-in-the-Sky/cgi-bin/
+	tar -cvf cgi-bin.tar .
 
+	echo "cleaning apache/cgi-bin"
+	cd /usr/local/apache/cgi-bin/
+	rm -rf * 
 
-echo "Tarring git repo"
-cd /home/pi/Pi-in-the-Sky/documentRoot/
-tar -cvf documentRoot.tar .
+	echo "Unpacking"
+	mv /home/pi/Pi-in-the-Sky/cgi-bin/cgi-bin.tar ./
+	tar -xvf cgi-bin.tar
 
-echo "cleaning documentroot"
-cd /usr/local/apache/htdocs/
-rm -rf * 
-
-echo "Unpacking"
-mv /home/pi/Pi-in-the-Sky/documentRoot/documentRoot.tar ./
-tar -xvf documentRoot.tar
-
-echo "Cleaning up"
-rm documentRoot.tar
+	echo "Cleaning up"
+	rm cgi-bin.tar
+fi
 
 echo "done!"
