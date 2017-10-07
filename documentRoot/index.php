6c12a18f30739cab6a61366a5a58c14cf6ce536f
Apara Venkat <ssapar@gmail.com>
2017-10-07 11:57:56 -0600

    Move to PHP from HTML

---
+++ documentRoot/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Pi In The Sky</title>
+	</head>
+	<body>
+		<h1>Pi In The Sky</h1>
+		<p>Pi in the Sky homepage</p>
+		<p>This line means that the website is properly pulling content from github</p>
+		<p>This line is for testing a cleaner script</p>
+	</body>
+</html>
