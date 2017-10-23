5db11d3063fc5260a16fe28dc458c6b67dad7b43
Apara Venkat <ssapar@gmail.com>
2017-10-23 14:20:13 -0600

    Split websiteUpload.php into its HTML and PHP counterparts

--- documentRoot/includes/website_upload.php
+++ documentRoot/includes/website_upload.php
@@ -1,16 +1,8 @@
+<!-- Website Upload module -->
 <?php
-//If not coming from submit page:
-//	go to login page
-session_start();
-if (!isset($_SESSION['username']))
-{
-	$_SESSION['msg'] = "You must log in first";
-	header('location: index.php');
-}
 
-//once button is clicked
-if(isset($_POST['submit']))
-{
+// If 'upload-website' button is clicked
+if(isset($_POST['upload-website'])) {
 	//get file name
 	$name = $_FILES['siteFile']['name'];
 	//get file tmpName
@@ -23,22 +15,19 @@ if(isset($_POST['submit']))
 	$siteName = $_POST['siteName'];
 
 	//if type is not an allowable type:
-	if(!($type == "application/zip" || $type == "text/html"))
-	{
+	if(!($type == "application/zip" || $type == "text/html")) {
 		//exit, report error
 		die($type." Is not a valid file type. Must be either .html or .zip");
 	}
 
 	//If site name is a domain name:
-	if($_POST['domain'] == 'true')
-	{
+	if($_POST['domain'] == 'true') {
 		//check if valid domain
 		$realIP = file_get_contents("http://ipecho.net/plain");
 		$givenIP = gethostbyname($siteName);
 
 		//check that domain points to server IP
-		if($givenIP != $realIP)
-		{
+		if($givenIP != $realIP) {
 			die("That domain does not point to this server");
 		}
 	}
@@ -55,38 +44,31 @@ if(isset($_POST['submit']))
 	//create directory
 	exec('mkdir /var/userSites/'.escapeshellarg($siteName).'/');
 	//Upload file to new directory
-	if(move_uploaded_file($_FILES['siteFile']['tmp_name'], '/var/userSites/'.$siteName.'/'.$name))
-	{
+	if(move_uploaded_file($_FILES['siteFile']['tmp_name'], '/var/userSites/'.$siteName.'/'.$name)) {
 		//unzip zipped files
-		if($type == "application/zip")
-		{
+		if($type == "application/zip") {
 			$zip = new ZipArchive;
-			if($zip->open('/var/userSites/'.$siteName.'/'.$name))
-			{
+			if($zip->open('/var/userSites/'.$siteName.'/'.$name)) {
 				$zip->extractTo('/var/userSites/'.$siteName.'/');
 				$zip->close();
 			}
-			else
-			{
+			else {
 				echo 'unzip failed';
 			}
 		}
-		else
-		{
+		else {
 			//If just one file, make it the index file
 			exec('mv /var/userSites/'.$siteName.'/'.$name.' /var/userSites/'.$siteName.'/index.html');
 		}
 	}
-	else
-	{
+	else {
 		//if something went wrong, remove the directory
 		exec('rmdir /var/userSites/'.escapeshellarg($siteName).'/');
 		echo "There was an issue uploading your file";
 	}
 
 	//If site name is a domain name:
-	if($_POST['domain'] == 'true')
-	{
+	if($_POST['domain'] == 'true') {
 		//add entry to sites-available with domain name and document root
 		$confFile = fopen('/etc/apache2/sites-available/'.$siteName.'.conf','w');
 		fwrite($confFile,"<VirtualHost *:80>\n");
@@ -107,8 +89,7 @@ if(isset($_POST['submit']))
 		fwrite($confFile,"</VirtualHost>");
 		fclose($confFile);
 	}
-	else
-	{
+	else {
 		//add directory and alias to new sites-available conf file
 		$confFile = fopen('/etc/apache2/sites-available/'.$siteName.'.conf','w');
 		fwrite($confFile,'Alias /'.$siteName.' "/var/userSites/'.$siteName.'/"');
@@ -127,36 +108,5 @@ if(isset($_POST['submit']))
 
 	//restart apache
 	exec('sudo apache2ctl -k graceful');
-}
+
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Website Upload | Pi In The Sky</title>
-	<link rel="stylesheet" type="text/css" href="/css/style.css">
-</head>
-<body>
-	<div class="header">
-		<h2>Website Upload<h2>
-	</div>
-	<div class="content">
-	<p>Here you can upload files to be hosted as a new website!<br>
-	You can either upload a single html document or a .zip of a website directory<br>
-	If you have a domain name for your website, it will be hosted there. <br>
-	Otherwise just give your site a name and it'll be hosted at "this_ip/websitename"<br>
-	<br>
-	If you are uploading a .zip make sure that the page you want a visitor to see first is called "index.html" or any other web accessible file extension.<br></p>
-	<!-- Form -->
-	<form action="websiteUpload.php" method="post" enctype="multipart/form-data">
-		Select File to Upload:
-		Site name:<input type="text" name="siteName"><br>
-		Is this a domain name?<br>
-		<input type="radio" name="domain" value="true">Yes<br>
-		<input type="file" name="siteFile" id="siteFile"><br>
-		<input type="submit" value="Upload File" name="submit">
-	</form>
-
-	<div>
-</body>
-</html>
---
+++ documentRoot/upload.php
@@ -0,0 +1,43 @@
+<?php
+	session_start();
+
+	// Check whether the user is logged in
+	if (!isset($_SESSION['username'])) {
+		$_SESSION['msg'] = "You must log in first";
+		header('location: index.php');
+	}
+
+	// Includes logout module.
+	include('includes/server.php');
+?>
+<!-- Website Upload Page -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Website Upload | Pi In The Sky</title>
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Website Upload<h2>
+	</div>
+	<div class="content">
+	<p>Here you can upload files to be hosted as a new website!<br>
+	You can either upload a single html document or a .zip of a website directory<br>
+	If you have a domain name for your website, it will be hosted there. <br>
+	Otherwise just give your site a name and it'll be hosted at "this_ip/websitename"<br>
+	<br>
+	If you are uploading a .zip make sure that the page you want a visitor to see first is called "index.html" or any other web accessible file extension.<br></p>
+	<!-- Form -->
+	<form action="websiteUpload.php" method="post" enctype="multipart/form-data">
+		<h3>Select File to Upload:</h3>
+		Site name:&nbsp;&nbsp;<input type="text" name="siteName"><br><br>
+		Is this a domain name?<br>
+		<input type="radio" name="domain" value="true">Yes<br><br>
+		<input type="file" name="siteFile" id="siteFile"><br><br>
+		<input type="submit" value="Upload Website" name="upload-website">
+	</form>
+
+	<div>
+</body>
+</html>
