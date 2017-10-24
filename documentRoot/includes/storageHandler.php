df82543d82455da315737d948ccb71d90dd7a878
Jared Cantilina <jared.cantilina@colorado.edu>
2017-10-23 18:00:05 -0600

    started work on cloud page

---
+++ documentRoot/includes/storageHandler.php
@@ -0,0 +1,23 @@
+<?php
+//If not logged in, redirect to login page
+//
+//connect to MySQL instance
+//
+//If uploading a file:
+	//get file name
+	//get file type
+	//get file size
+	//get file 
+
+	//check file specifics
+	//store metadata in mysql
+	//move to private or public storage
+
+//If torrenting file:
+	//get torrent URL
+	//get file data from transmission?
+	//start torrent with transmission
+
+//
+
+?>
--- documentRoot/storage.php
+++ documentRoot/storage.php
@@ -1,23 +1,31 @@
 <?php
-//If not logged in, redirect to login page
-//
-//connect to MySQL instance
-//
-//If uploading a file:
-	//get file name
-	//get file type
-	//get file size
-	//get file 
-
-	//check file specifics
-	//store metadata in mysql
-	//move to private or public storage
-
-//If torrenting file:
-	//get torrent URL
-	//get file data from transmission?
-	//start torrent with transmission
-
-//
-
+if (!isset($_SESSION['username'])){
+	$_SESSION['msg'] = "You must log in first";
+	header("location: index.php");
+}
+include('includes/server.php');
 ?>
+
+<html>
+<head>
+	<title>Cloud Storage | Pi In The Sky</title>
+</head>
+<body>
+	<div class="header">
+		<h2>Cloud Storage</h2>
+	</div>
+	<div class="content">
+		<p>Here you can upload files to your server!<br>
+		You can upload files from your computer, or specify a bit torrent URL for your server to download.<br>
+		You can also specify if you want this file to be available to only you, or everyone.</p>
+		
+		<form action="storageHandler.php" method="post" enctype="multipart/form-data">
+			Select file to upload:<input type="file" name="uploadFile" id="uploadFile"><br>
+			Or specify a bitTorrent URL:<input type="text" name="TorURL"><br>
+			Is this a private or public file:<input type="radio" name="privacy" value="private">
+			<input type="radio" name="privacy" value="public"><br>
+			<input type="submit" name="upload-submit" value="Upload File">
+		</form>
+		<!-- Display uploaded files here -->
+	</div>
+</body>
