f0e7ea62fc7c1748c73f498e1064740f94ed9c2f
jaredtcan <jared.cantilina@colorado.edu>
2017-11-02 16:27:21 +0000

    test for delete handler is working

---
+++ documentRoot/includes/storageDelete.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if(!isset($_SESSION['username'])){
+	$_SESSION['msg']="You must log in first!";
+	header("location: index.php");
+}
+include('server.php');
+
+if(isset($_POST['publicFile'])){
+	$file = '/var/data/public/'.$_POST['publicFile'];
+}else if(isset($_POST['privateFile'])){
+	$file = '/var/data/'.$_SESSION['username'].'/'.$_POST['privateFile'];
+}else{
+	die("something went horribly wrong!!");
+}
+echo("<p>".$file."</p>");
+
+?>
