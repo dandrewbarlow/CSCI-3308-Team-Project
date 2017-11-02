1942ebac7325558394148a162ad9b94541090d65
jaredtcan <jared.cantilina@colorado.edu>
2017-11-02 00:50:09 +0000

    got site enabling working

--- documentRoot/includes/siteEnable.php
+++ documentRoot/includes/siteEnable.php
@@ -8,8 +8,11 @@ include('server.php');
 
 $siteName = $_POST['siteID'];
 
-
-$sqlquery = 'UPDATE websites SET is_enabled = 1 WHERE website_name="'.$siteName.'"';
-mysqli_query($conn, $sqlquery);
-header("location: ../userSites.php");
+if(symlink('/etc/apache2/sites-available/'.$siteName.'.conf','/etc/apache2/sites-enabled/'.$siteName.'.conf')){
+	$sqlquery = 'UPDATE websites SET is_enabled = 1 WHERE website_name="'.$siteName.'"';
+	mysqli_query($conn, $sqlquery);
+	header("location: ../userSites.php");
+}else{
+	die("something went wrong. The site data was probably deleted improperly");
+}
 ?>
