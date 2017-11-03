40cb9f1b7e4230d7a855e9ba1f24c9003e284479
Apara Venkat <ssapar@gmail.com>
2017-11-03 15:24:00 -0600

    Debugging 1

--- documentRoot/userSites.php
+++ documentRoot/userSites.php
@@ -53,37 +53,37 @@ include('includes/dbconnect.php');
 		<h2>Available Sites</h2>
 		<table align="center">
 		<?php
-			$query = ""
-			if ($_SESSION['superuser'] == 1) {
-				$query = "SELECT * FROM websites WHERE is_enabled = 0";
-			}
-			else {
-				$username = $_SESSION['username'];
-				$query = "SELECT * FROM websites WHERE (is_enabled = 0 AND user_uid='$username')";
-			}
-			$result = mysqli_query($conn, $query);
-			$resultCheck = mysqli_num_rows($result);
-			if ($resultCheck == 0) {
-				echo "There no other available sites right now.";
-			}
-			else {
-				$filelist = "<tr>";
-				$filelist .= "<th>Id</th><th>Website Name</th><th>Created By</th><th>Created On</th><th>Is Domain?</th><th>Enable?</th><th>Delete?</th>";
-				$filelist .= "</tr>";
-				while($row = mysqli_fetch_array($result)){
-					$form = '<form method="post" action="includes/siteEnable.php"><input type="hidden" name="siteID" value="'.$row['site_id'].'">';
-					$button = '<input type="submit" value="Enable"></form>';
-					$form2 = '<form method="post" action="includes/siteDelete.php"><input type="hidden" name="siteID" value="'.$row['site_id'].'">';
-					$button2 = '<input type="submit" value="Delete"></form>';
-					$filelist .= "<tr><td>".$row['site_id'].'</td>';
-					$filelist .= "<td>".$row['website_name'].'</td>';
-					$filelist .= "<td>".$row['user_uid'].'</td>';
-					$filelist .= "<td>".$row['created_on'].'</td>';
-					$filelist .= "<td>".$row['is_domain'].'</td>';
-					$filelist .= '<td>'.$form.$button.'</td><td>'.$form2.$button2.'</td></tr><br>';
-				}
-				echo $filelist;
-			}
+			// $query = ""
+			// if ($_SESSION['superuser'] == 1) {
+			// 	$query = "SELECT * FROM websites WHERE is_enabled = 0";
+			// }
+			// else {
+			// 	$username = $_SESSION['username'];
+			// 	$query = "SELECT * FROM websites WHERE (is_enabled = 0 AND user_uid='$username')";
+			// }
+			// $result = mysqli_query($conn, $query);
+			// $resultCheck = mysqli_num_rows($result);
+			// if ($resultCheck == 0) {
+			// 	echo "There no other available sites right now.";
+			// }
+			// else {
+			// 	$filelist = "<tr>";
+			// 	$filelist .= "<th>Id</th><th>Website Name</th><th>Created By</th><th>Created On</th><th>Is Domain?</th><th>Enable?</th><th>Delete?</th>";
+			// 	$filelist .= "</tr>";
+			// 	while($row = mysqli_fetch_array($result)){
+			// 		$form = '<form method="post" action="includes/siteEnable.php"><input type="hidden" name="siteID" value="'.$row['site_id'].'">';
+			// 		$button = '<input type="submit" value="Enable"></form>';
+			// 		$form2 = '<form method="post" action="includes/siteDelete.php"><input type="hidden" name="siteID" value="'.$row['site_id'].'">';
+			// 		$button2 = '<input type="submit" value="Delete"></form>';
+			// 		$filelist .= "<tr><td>".$row['site_id'].'</td>';
+			// 		$filelist .= "<td>".$row['website_name'].'</td>';
+			// 		$filelist .= "<td>".$row['user_uid'].'</td>';
+			// 		$filelist .= "<td>".$row['created_on'].'</td>';
+			// 		$filelist .= "<td>".$row['is_domain'].'</td>';
+			// 		$filelist .= '<td>'.$form.$button.'</td><td>'.$form2.$button2.'</td></tr><br>';
+			// 	}
+			// 	echo $filelist;
+			// }
 		?>
 		</table>
 		<br><br>
