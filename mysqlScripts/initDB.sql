278631a9b060e8643701918a3367bd3c09877303
Apara Venkat <ssapar@gmail.com>
2017-11-02 18:38:18 -0600

    Add new column to users table

--- mysqlScripts/initDB.sql
+++ mysqlScripts/initDB.sql
@@ -1,3 +1,3 @@
-insert into users (user_id, user_name, user_email, user_uid, psswd)
+insert into users (user_id, user_name, user_email, user_uid, psswd, superuser)
 	values
-	(1, 'Admin', '', 'god', 'be39f9ab6be772aa81422ea983664c9e');
+	(1, 'Admin', '', 'god', 'be39f9ab6be772aa81422ea983664c9e', 1);
