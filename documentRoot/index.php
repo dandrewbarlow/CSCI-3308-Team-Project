86681013443ce876cfa648b18c9fd8ee8cfd2116
Apara Venkat <ssapar@gmail.com>
2017-11-04 14:51:15 -0600

    Bootstrap for index.php

--- documentRoot/index.php
+++ documentRoot/index.php
@@ -15,39 +15,58 @@
 		<meta charset="utf-8">
 		<title>Login | Pi In The Sky</title>
 		<link rel="stylesheet" href="/css/style.css">
+		<link rel="stylesheet" href="/css/bootstrap.css">
 	</head>
 
 	<body>
-		<div class="header">
-			<h1>Pi In the Sky</h1>
-		</div>
-		<div class="extra-space"></div>
 
-		<div class="login-form">
-			<h2>Login</h2>
+		<div class="extra-space-sm"></div>
 
-			<?php include('includes/errors.php') ?>
-
-			<form class="login" action="index.php" method="post">
-				<div class="input-group">
-					<label>Username</label>&nbsp;
-					<input type="text" name="username" value="">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-8 header">
+					<h1>Pi In the Sky</h1>
 				</div>
-				<div class="input-group">
-					<label>Password</label>&nbsp;
-					<input type="password" name="password" value="">
-				</div>
-				<div class="input-group">
-					<button type="submit" name="login" class="btn">Login</button>
-				</div>
-			</form>
-
+				<div class="col-md-2"></div>
+			</div>
 		</div>
 
-		<div class="footer" style="padding: 10px;">
-			&copy; The Pi.in.the.Sky Team
+		<div class="extra-space-md"></div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-8">
+					<div class="login-form">
+						<h2>Login</h2>
+						<div class="extra-space-sm"></div>
+						<?php include('includes/errors.php') ?>
+
+						<form class="login" action="index.php" method="post">
+							<div class="form-group">
+								<label>Username</label>&nbsp;
+								<input type="text" name="username" value="">
+							</div>
+							<div class="form-group">
+								<label>Password</label>&nbsp;
+								<input type="password" name="password" value="">
+							</div>
+							<div class="form-group">
+								<button type="submit" name="login" class="btn btn-primary">Login</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
 		</div>
 
+    <div class="footer">
+        <div class="container text-muted center">
+			© 2017 Pi In The Sky
+        </div>
+    </div>
 	</body>
 
 </html>
