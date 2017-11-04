3bde6c05949e4a7896912bf4b9dcfc2aab886ec6
Apara Venkat <ssapar@gmail.com>
2017-11-04 17:10:57 -0600

    Redesign website management pages

--- documentRoot/upload.php
+++ documentRoot/upload.php
@@ -10,33 +10,88 @@
 <head>
 	<title>Website Upload | Pi In The Sky</title>
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-	<div class="header">
-		<h2>Website Upload<h2>
-	</div>
-	<div class="content">
-	<p>Here you can upload files to be hosted as a new website!<br>
-	You can either upload a single html document or a .zip of a website directory<br>
-	If you have a domain name for your website, it will be hosted there. <br>
-	Otherwise just give your site a name and it'll be hosted at "this_ip/websitename"<br>
-	<br>
-	If you are uploading a .zip make sure that the page you want a visitor to see first is called "index.html" or any other web accessible file extension.<br></p>
-	<!-- Form -->
-	<form action="includes/website_upload.php" method="post" enctype="multipart/form-data">
-		<?php
-			if (isset($_SESSION['username'])) {
-				echo "Username: <input type=\"text\" name=\"username\" value=".$_SESSION['username']." disabled><br><br>";
-			}
-		?>
-		<h3>Select File to Upload:</h3>
-		Site name:&nbsp;&nbsp;<input type="text" name="siteName"><br><br>
-		Is this a domain name?<br>
-		<input type="radio" name="domain" value="true">Yes<br><br>
-		<input type="file" name="siteFile" id="siteFile"><br><br>
-		<input type="submit" value="Upload Website" name="upload-website">
-	</form>
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="home.php">Pi In The Sky</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link" href="home.php">Home</a>
+				<a class="nav-item nav-link" href="userSites.php">Manage Websites</a>
+				<a class="nav-item nav-link" href="storage.php">Cloud Storage</a>
+				<a class="nav-item nav-link" href="reset.php">Reset Password</a>
+				<a class="nav-item nav-link" href="includes/logout.php">Logout</a>
+			</div>
+		</div>
+	</nav>
+	<!-- End navbar -->
 
-	<div>
+	<div class="extra-space-sm"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8 header">
+				<h1>Upload New Website</h1><br>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<p>
+					Here you can upload files to be hosted as a new website!<br>
+					You can either upload a single html document or a .zip of a website directory<br>
+					If you have a domain name for your website, it will be hosted there. <br>
+					Otherwise just give your site a name and it'll be hosted at "this_ip/websitename"<br><br>
+					If you are uploading a .zip make sure that the page you want a visitor to see first is called "index.html" or any other web accessible file extension.
+				</p><br>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8 center">
+				<form action="includes/website_upload.php" method="post" enctype="multipart/form-data">
+					<h4>Select File to Upload:</h4>
+					<table align="center">
+						<tr>
+							<td>Site name</td>
+							<td><input type="text" name="siteName"></td>
+						</tr>
+						<tr>
+							<td>Is this a domain name?</td>
+							<td><input type="radio" name="domain" value="true">Yes</td>
+						</tr>
+					</table><br>
+					<label for="siteFile" class="btn btn-primary">Upload File</label>
+					<input type="file" name="siteFile" id="siteFile" style="display: none;"><br><br>
+					<input class="btn btn-success" type="submit" value="Upload Website" name="upload-website">
+				</form>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<!-- Form -->
+
+	</div>
+
+	<!-- Footer -->
+	<div class="footer">
+	    <div class="container text-muted center">
+			© 2017 Pi In The Sky
+	    </div>
+	</div>
+	<!-- End footer -->
 </body>
 </html>
--- documentRoot/userSites.php
+++ documentRoot/userSites.php
@@ -8,14 +8,45 @@ include('includes/dbconnect.php');
 <head>
 	<title>Hosted Sites | Pi In The Sky</title>
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-	<div class="header">
-		<h2>Website Management</h2>
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="home.php">Pi In The Sky</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link" href="home.php">Home</a>
+				<a class="nav-item nav-link active" href="#">Manage Websites <span class="sr-only">(current)</span></a>
+				<a class="nav-item nav-link" href="storage.php">Cloud Storage</a>
+				<a class="nav-item nav-link" href="reset.php">Reset Password</a>
+				<a class="nav-item nav-link" href="includes/logout.php">Logout</a>
+			</div>
+		</div>
+	</nav>
+	<!-- End navbar -->
+
+	<div class="extra-space-sm"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8 header">
+				<h1>Website Management</h1>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
 	</div>
-	<div class="content">
-		<p>Here you can manage what websites your sever is currently hosting</p>
-		<h2>Enabled Sites</h2>
+
+	<div class="description">
+		<p>Manage your websites the sever is currently hosting</p><br>
+		<a class="btn btn-primary" href="upload.php">Create a new website</a><br><br>
+		<h2>Enabled Sites</h2><br>
 		<table align="center">
 		<?php
 			$query = "";
@@ -46,8 +77,8 @@ include('includes/dbconnect.php');
 				echo $filelist;
 			}
 		?>
-		</table>
-		<h2>Available Sites</h2>
+		</table><br><br>
+		<h2>Available Sites</h2><br>
 		<table align="center">
 		<?php
 			$query = "";
@@ -82,9 +113,15 @@ include('includes/dbconnect.php');
 				echo $filelist;
 			}
 		?>
-		</table>
-		<br><br>
-		<a href="upload.php">Create a site</a>
+		</table><br><br>
 	</div>
+
+	<!-- Footer -->
+	<div class="footer">
+	    <div class="container text-muted center">
+			© 2017 Pi In The Sky
+	    </div>
+	</div>
+	<!-- End footer -->
 </body>
 </html>
