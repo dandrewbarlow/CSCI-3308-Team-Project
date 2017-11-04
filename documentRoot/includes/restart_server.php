2a3cf5c556e7b3bd98dcd4d6c58345755ad1d9d0
JaredTcan <jared.cantilina@colorado.edu>
2017-11-04 01:17:09 -0600

    Those damn semicolons

--- documentRoot/includes/restart_server.php
+++ documentRoot/includes/restart_server.php
@@ -10,9 +10,5 @@ if(isset($_SESSION['username']))
 	exec('sudo /sbin/reboot');
 }
 //if something went wrong and we're still alive, go to index
-<<<<<<< HEAD
-header('location: ../index.php')
-=======
-header('location: /index.php')
->>>>>>> 13199a9a7f783dc2023bd8149e10bfceffa8f24c
+header('location: ../index.php');
 ?>
