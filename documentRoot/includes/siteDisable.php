963b43b49ebf6cf8e8e1b756ea66171436dbc13f
jaredtcan <jared.cantilina@colorado.edu>
2017-11-04 05:59:34 +0000

    fixed more bugs ;

--- documentRoot/includes/siteDisable.php
+++ documentRoot/includes/siteDisable.php
@@ -1,16 +1,12 @@
 <?php
 session_start();
-if(!isset($_SESSION['username'])){
-	$_SESSION['msg']="You must log in first!";
-	header("location: index.php");
-}
 include('requireLogin.php');
 include('dbconnect.php');
 
-$siteId = $_POST['siteID'];
+$siteID = $_POST['siteID'];
 
-unlink('/etc/apache2/sites-enabled/'.$siteName.'.conf');
-$sqlquery = 'UPDATE websites SET is_enabled = 0 WHERE site_id="'.$siteId.'"';
+unlink('/etc/apache2/sites-enabled/'.$siteID.'.conf');
+$sqlquery = 'UPDATE websites SET is_enabled = 0 WHERE site_id="'.$siteID.'"';
 mysqli_query($conn, $sqlquery);
 exec('sudo apache2ctl -k graceful');
 header("location: ../userSites.php");
