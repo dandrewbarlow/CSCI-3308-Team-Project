45ba725878916767564dce648ab62fef850490b8
jaredtcan <jared.cantilina@colorado.edu>
2017-11-12 17:34:57 +0000

    added quotes around sql phrase

--- documentRoot/includes/storageHandler.php
+++ documentRoot/includes/storageHandler.php
@@ -31,7 +31,7 @@ if(isset($_POST['storage-submit'])){
 		//check file specifics
 		//move to private or public storage
 		if(move_uploaded_file($tmpName, $storageDir.$name)){
-			$sql='INSERT INTO storage (name, user_name, prefix, is_private, stored_on) VALUES ('.$name.','.$username.','.$storageDir.','.$is_private.','.date("Y-m-d H:i:s").')';
+			$sql='INSERT INTO storage (name, user_name, prefix, is_private, stored_on) VALUES ("'.$name.'","'.$username.'","'.$storageDir.'",'.$is_private.','.date("Y-m-d H:i:s").')';
 			mysqli_query($conn, $sql);
 		}else{
 			die($name." file upload unsuccessful. Please go back and try again");
