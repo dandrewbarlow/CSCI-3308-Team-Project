d8a498afd1a72ce291b45d2dfbc1692a543bacfd
JaredTcan <jared.cantilina@colorado.edu>
2017-11-25 14:00:45 -0700

    Fixed semicolon issue

--- documentRoot/includes/errors.php
+++ documentRoot/includes/errors.php
@@ -3,9 +3,9 @@
 session_start();
 if (isset($_SESSION['error'])){
 	echo '<div id="error">'.$_SESSION['error'].'</div>';
-	unset($_SESSION['error'])
+	unset($_SESSION['error']);
 } elseif (isset($_SESSION['success'])) {
 	echo '<div id="success">'.$_SESSION['success'].'</div>';
-	unset($_SESSION['success'])
+	unset($_SESSION['success']);
 }
 ?>
