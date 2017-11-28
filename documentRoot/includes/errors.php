ae03175d699ebc9042a396d9a87150d1dc0fc187
Apara Venkat <ssapar@gmail.com>
2017-11-28 16:12:09 -0700

    Stylize messages

--- documentRoot/includes/errors.php
+++ documentRoot/includes/errors.php
@@ -1,18 +1,13 @@
 <!-- Display errors that any input form has -->
 <?php
-if (isset($_SESSION['error'])){
-	echo '<div id="error">'.$_SESSION['error'].'</div>';
-	unset($_SESSION['error']);
-}
-if (isset($_SESSION['success'])){
-	echo '<div id="success">'.$_SESSION['success'].'</div>';
-	unset($_SESSION['success']);
-}
+	session_start();
+	
+	if (isset($_SESSION['error'])) {
+		echo '<div class="error-msg">'.$_SESSION['error'].'</div>';
+		unset($_SESSION['error']);
+	}
+	if (isset($_SESSION['success'])) {
+		echo "<div class='success-msg'>".$_SESSION['success']."</div>";
+		unset($_SESSION['success']);
+	}
 ?>
-<?php  if (count($errors) > 0) : ?>
-	<div class="error-msg">
-		<?php foreach ($errors as $error) : ?>
-			<p><?php echo $error ?></p>
-		<?php endforeach ?>
-	</div>
-<?php  endif ?>
